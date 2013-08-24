456d00010528aee882d002a5eff44bb1913422f1
Patrick Higgins <quickpanic@me.com>
2013-08-24 00:18:44 -0700

    Added LibraryLink
    Created a placeholder for the future LibraryLink C++ File, to connect
    to the quickfx C++ library.

---
+++ LibraryLink.cpp
@@ -0,0 +1,9 @@
+//
+//  LibraryLink.cpp
+//  AdvancedNumbers
+//
+//  Created by Patrick Higgins on 8/24/13.
+//  Copyright (c) 2013 Patrick is Really Cool. All rights reserved.
+//
+
+#include "LibraryLink.h"
---
+++ LibraryLink.h
@@ -0,0 +1,14 @@
+//
+//  LibraryLink.h
+//  AdvancedNumbers
+//
+//  Created by Patrick Higgins on 8/24/13.
+//  Copyright (c) 2013 Patrick is Really Cool. All rights reserved.
+//
+
+#ifndef __AdvancedNumbers__LibraryLink__
+#define __AdvancedNumbers__LibraryLink__
+
+#include <iostream>
+
+#endif /* defined(__AdvancedNumbers__LibraryLink__) */
